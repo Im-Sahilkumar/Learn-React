0d221290c1965e066bfcb1624d61faec9f1ba6c8
Im-Sahilkumar <sahilrajsahil28@gmail.com>
2026-01-15 01:21:17 +0530

    hicommit

---
+++ 1.practice/src/App.jsx
@@ -0,0 +1,12 @@
+import 
+function First (){
+  return(
+    <div>
+      <p>
+        Hello World
+      </p>
+    </div>
+  )
+}
+export default First;
+
---
+++ 2.practice/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './usercomp'
+function Head (){
+  return(
+    <div>
+    <p>
+      Hi There.?
+    </p>
+    <Login / >
+    </div>
+  )
+}
+export default Head;
